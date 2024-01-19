e626c1df3b1f97f90d69f18215a243cc55a6cd83
jieun <jieun_0330@naver.com>
2024-01-19 21:38:48 +0900

    :sparkles: [feat] 닉네임 조건 구현중

--- SeSAC_Recap/NicknameViewController.swift
+++ SeSAC_Recap/NicknameViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class NicknameViewController: UIViewController {
+class NicknameViewController: UIViewController, UITextFieldDelegate {
     
     @IBOutlet var profileImg: UIImageView!
     @IBOutlet var cameraImg: UIImageView!
@@ -15,27 +15,80 @@ class NicknameViewController: UIViewController {
     @IBOutlet var nicknameCondition: UILabel!
     @IBOutlet var doneButton: UIButton!
     
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+//        let nickname = UserDefaults.standard.string(forKey: "Nickname")
+//        
+//        if let nickname {
+//            nicknameCondition.text = nickname
+//        }
+
         configureUI()
-    }
-    @IBAction func nickNameTextFieldTapped(_ sender: UITextField) {
-        if sender.text!.count < 2 || sender.text!.count > 10 {
-            nicknameCondition.text = "2글자 이상 10글자 미만으로 설정해주세요"
-        }
+
+        nicknameTextField.delegate = self
+        nicknameTextField.smartDashesType = .no
+        
+        
+
+        
+        
     }
     
-    //Mark: - image - TapGeture 연결 정리하기
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        if string == "#@$%" {
+            return false
+        }
+        return true
+    }
+    
+
+    
+    
+    
+    @IBAction func nickNameTextFieldTapped(_ sender: UITextField) {
+        
+//        let nickname = sender.text
+//        let specialCharacters = ["#", "@", "$", "%"]
+        
+        if let nickname = Int(sender.text!) {
+            nicknameCondition.text = "닉네임에 숫자는 포함할 수 없어요"
+        } else if sender.text!.count < 2 || sender.text!.count > 10 {
+            nicknameCondition.text = "2글자 이상 10글자 미만으로 설정해주세요"
+        }
+//        } else if sender.text?.contains("#@$%") == nil {
+//            nicknameCondition.text = "kk"
+////            sender.smartDashesType
+//        }
+        
+        if let text = sender.text?.components(separatedBy: "").contains(["#", "@", "$", "%"]) {
+            if text {
+                nicknameCondition.text = "3"
+            }
+        }
+
+
+        UserDefaults.standard.set(nicknameTextField.text, forKey: "Nickname")
+
+    }
+
     @IBAction func imageClicked(_ sender: UITapGestureRecognizer) {
         let vc = storyboard?.instantiateViewController(identifier: SettingProfileViewController.identifier) as! SettingProfileViewController
         navigationController?.pushViewController(vc, animated: true)
     }
     
-    
+    @IBAction func keyboardDismiss(_ sender: UITapGestureRecognizer) {
+        view.endEditing(true)
+        UserDefaults.standard.set(nicknameTextField.text, forKey: "Nickname")
+
+    }
+
 
     
 }
+
+
 
 extension NicknameViewController {
     func configureUI() {
@@ -43,23 +96,18 @@ extension NicknameViewController {
         
         let randomNum = Int.random(in: 1...14)
         profileImg.image = UIImage(named: "profile\(randomNum)")
-        
-        //Mark: - 이미지 동그랗게 하는 방법 정리하기
-        //Mark: - Autolayout 개념 정리하기
-        //Mark: - 이미지를 클릭했을 때 ? -> 버튼으로 바꿔야하나?
+
         profileImg.contentMode = .scaleAspectFill
         profileImg.layer.masksToBounds = false
         profileImg.layer.cornerRadius = profileImg.frame.height / 2
         profileImg.clipsToBounds = true
-        profileImg.layer.borderWidth = 1
-        //Mark: - borderColor = CGColor
+        profileImg.layer.borderWidth = 5
         profileImg.layer.borderColor = Colors.pointColor.cgColor
         cameraImg.image = .camera
         
         nicknameTextField.placeholder = "닉네임을 입력해주세요 :)"
         nicknameTextField.font = Fonts.font13
         
-        nicknameCondition.text = "닉네임에 뭐가 포함될 수 없대"
         nicknameCondition.textColor = Colors.pointColor
         nicknameCondition.font = Fonts.font13
         
