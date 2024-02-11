4f3d671f58547e862f565d294a9b0df4fcd95b4e
jieun <jieun_0330@naver.com>
2024-02-11 15:04:38 +0900

    :recycle: [refactor] 프로필닉네임설정화면 코드정리

--- SeSAC_Recap/Profile/NicknameViewController.swift
+++ SeSAC_Recap/Profile/NicknameViewController.swift
@@ -21,13 +21,13 @@ class NicknameViewController: UIViewController, UITextFieldDelegate {
         super.viewDidLoad()
         
         configureUI()
-
+        
     }
     
     func configureUI() {
         
         navigationItem.title = "프로필 설정"
-
+        
         UserDefaultManager.shared.profileImg = randomNum
         profileImg.image = UIImage(named: "profile\(UserDefaultManager.shared.profileImg)")
         configureProfileImage(profileImg)
@@ -36,6 +36,7 @@ class NicknameViewController: UIViewController, UITextFieldDelegate {
         
         nicknameTextField.placeholder = "닉네임을 입력해주세요 :)"
         nicknameTextField.font = Fonts.font13
+        UserDefaultManager.shared.nickname = nicknameTextField.text!
         
         nicknameCondition.textColor = UIColor.pointColor
         nicknameCondition.font = Fonts.font13
@@ -49,41 +50,33 @@ class NicknameViewController: UIViewController, UITextFieldDelegate {
         doneButton.isEnabled = false
         
         navigationItem.backButtonTitle = ""
-        
-        UserDefaultManager.shared.nickname = nicknameTextField.text!
-        
-//        nicknameTextField.text = UserDefaults.standard.string(forKey: "Nickname")
     }
-
     
     override func viewWillAppear(_ animated: Bool) {
-//        let num = UserDefaults.standard.integer(forKey: "clickImg")
         profileImg.image = UIImage(named: "profile\(UserDefaultManager.shared.profileImg)")
-        
     }
     
-    @objc func textFieldDidChange() {
+    @objc func textFieldDidChange(_ sender: UITextField, condition: UILabel, done: UIButton) {
         
         let num = CharacterSet(charactersIn: "0123456789")
         let char = CharacterSet(charactersIn: "#@$%")
-        // whitespace 작업 미완성
-        //        let whitespace = nicknameTextField.text?.trimmingCharacters(in: .whitespaces)
-        if nicknameTextField.text!.count < 2 || nicknameTextField.text!.count >= 10 {
-            nicknameCondition.text = "2글자 이상 10글자 미만으로 설정해주세요"
-            nicknameCondition.textColor = .red
-            doneButton.isEnabled = false
-        } else if ((nicknameTextField.text?.rangeOfCharacter(from: num)) != nil) {
-            nicknameCondition.text = "닉네임에 숫자는 포함할 수 없어요."
-        } else if ((nicknameTextField.text?.rangeOfCharacter(from: char)) != nil) {
-            nicknameCondition.text = "닉네임에 @,#,$,%는 포함할 수 없어요."
+        
+        if sender.text!.count < 2 || sender.text!.count >= 10 {
+            condition.text = "2글자 이상 10글자 미만으로 설정해주세요"
+            condition.textColor = .red
+            done.isEnabled = false
+        } else if ((sender.text?.rangeOfCharacter(from: num)) != nil) {
+            condition.text = "닉네임에 숫자는 포함할 수 없어요."
+        } else if ((sender.text?.rangeOfCharacter(from: char)) != nil) {
+            condition.text = "닉네임에 @,#,$,%는 포함할 수 없어요."
         } else {
-            nicknameCondition.text = ""
-            doneButton.isEnabled = true
+            condition.text = ""
+            done.isEnabled = true
         }
     }
     
     @objc func doneButtonClicked() {
-//        UserDefaults.standard.set(nicknameTextField.text, forKey: "Nickname")
+        
         UserDefaults.standard.setValue(true, forKey: "UserState")
         
         let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene
@@ -96,11 +89,10 @@ class NicknameViewController: UIViewController, UITextFieldDelegate {
         sceneDelegate?.window?.makeKeyAndVisible()
     }
     
-    @IBAction func imageClicked(_ sender: UITapGestureRecognizer) {
+    @IBAction func profileImageClicked(_ sender: UITapGestureRecognizer) {
         let vc = storyboard?.instantiateViewController(identifier: SettingProfileViewController.identifier) as! SettingProfileViewController
         
         navigationController?.pushViewController(vc, animated: true)
-        
     }
     
     @IBAction func keyboardDismiss(_ sender: UITapGestureRecognizer) {
