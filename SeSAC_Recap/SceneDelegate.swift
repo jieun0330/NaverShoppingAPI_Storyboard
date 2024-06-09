60821ee9da7d9a4f8c5db6b965464585633323e6
jieun <jieun_0330@naver.com>
2024-06-09 16:23:53 +0900

    style: 검색화면 UI 레이아웃 수정

--- SeSAC_Recap/SceneDelegate.swift
+++ SeSAC_Recap/SceneDelegate.swift
@@ -21,9 +21,8 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 //        window?.rootViewController = navigationController
 //        window?.makeKeyAndVisible()
         
-        
-        // 사용자가 처음 들어온 것처럼 설정
-//        if value == false {
+//         사용자가 처음 들어온 것처럼 설정
+        if value == false {
             guard let scene = (scene as? UIWindowScene) else { return }
             
             window = UIWindow(windowScene: scene)
@@ -34,17 +33,17 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
             
             window?.rootViewController = nav
             window?.makeKeyAndVisible()
-//        } else {
-//            guard let scene = (scene as? UIWindowScene) else { return }
-//            
-//            window = UIWindow(windowScene: scene)
-//            
-//            let sb = UIStoryboard(name: "Main", bundle: nil)
-//            let vc = sb.instantiateViewController(withIdentifier: "mainTabBarController") as! UITabBarController
-//            
-//            window?.rootViewController = vc
-//            window?.makeKeyAndVisible()
-//        }
+        } else {
+            guard let scene = (scene as? UIWindowScene) else { return }
+            
+            window = UIWindow(windowScene: scene)
+            
+            let sb = UIStoryboard(name: "Main", bundle: nil)
+            let vc = sb.instantiateViewController(withIdentifier: "mainTabBarController") as! UITabBarController
+            
+            window?.rootViewController = vc
+            window?.makeKeyAndVisible()
+        }
 
     }
 
